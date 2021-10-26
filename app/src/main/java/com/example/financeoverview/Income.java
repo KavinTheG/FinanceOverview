71882a096e249c2e803d9593bac33d748fbc2e7d
KavinTheG <kavin.balag@gmail.com>
2021-10-26 16:35:56 -0400

    Added a spinner (mutli-select option) on the income layout. Added some funcitonality, have not styled it yet.

--- app/src/main/java/com/example/financeoverview/Income.java
+++ app/src/main/java/com/example/financeoverview/Income.java
@@ -5,23 +5,47 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 
-public class Income extends AppCompatActivity {
+public class Income extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_revenue);
+        setContentView(R.layout.activity_income);
 
         String[] s1 = {"Canadian Tire", "REI.UN DIV"};
         String[] s2 = {"Hardware Associate", "test"};
 
-        RecyclerView recyclerView = findViewById(R.id.recyclyerView);
+
+        Spinner spinner = findViewById(R.id.income_spinner);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.income_type, android.R.layout.simple_spinner_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(Income.this);
+
+        RecyclerView recyclerView = findViewById(R.id.recyclyerViewExpense);
 
         RecyclerViewAdapter myAdapter = new RecyclerViewAdapter(this, s1, s2);
         recyclerView.setAdapter(myAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
 
     }
 }
